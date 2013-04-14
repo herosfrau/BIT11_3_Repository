2d2426f3f82371c119a21a6a2548ea6460b2a32b
BJ <herosfrau@gmx.de>
2013-04-14 13:08:15 +0200

    Installation

---
+++ 2013_04_13_Versionsverwaltung/2013_04_13_Versionsverwaltung/Program.cs
@@ -0,0 +1,15 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+
+namespace _2013_04_13_Versionsverwaltung
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+
+        }
+    }
+}
---
+++ 2013_04_13_Versionsverwaltung/2013_04_13_Versionsverwaltung/Test.cs
@@ -0,0 +1,11 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+
+namespace _2013_04_13_Versionsverwaltung
+{
+    class Test
+    {
+    }
+}
